9ee52c7ea143249b17eab9ede20614db929ba81f
Rene <cph-ra147@cphbusiness.dk>
2022-11-14 23:14:22 +0100

    Snart færdig med kurset

--- SearchEngine.UITests/HTMLInteractionTestClass.cs
+++ SearchEngine.UITests/HTMLInteractionTestClass.cs
@@ -2,6 +2,9 @@
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Support.UI;
+using Xunit.Abstractions;
 
 namespace SearchEngine.UITests
 {
@@ -10,6 +13,16 @@ namespace SearchEngine.UITests
     {
         private const string HomeUrl = "https://localhost:7017/";
         private const string PrivateUrl = "https://localhost:7017/Home/Privacy/";
+
+
+        private readonly ITestOutputHelper output;
+
+        public HTMLInteractionTestClass(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+
 
         [Fact]
         public void GoToPrivacyPageByLink()
@@ -48,5 +61,146 @@ namespace SearchEngine.UITests
         }
 
 
+        [Fact]
+        public void GoToPrivacyByClassLink()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause();
+                IWebElement privacyLinkByClassName = driver.FindElement(By.ClassName("ToPortalClass"));
+                privacyLinkByClassName.Click();
+
+                TestHelper.Pause();
+
+                Assert.Equal("Privacy Policy - WebApplication1", driver.Title);
+                Assert.Equal(PrivateUrl, driver.Url);
+            }
+        }
+
+
+        [Fact]
+        public void DateHeaderInMyRulingTables()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause();
+                driver.Manage().Window.Maximize();
+                IWebElement headerNameCell = driver.FindElement(By.ClassName("header-dato"));
+               
+                string headerName = headerNameCell.Text;
+                TestHelper.Pause();
+
+                Assert.Equal("Dato", headerName);
+
+            }
+        }
+
+        [Fact]
+        public void AllHeadersInMyRulingTables()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                //TestHelper.Pause();
+                driver.Manage().Window.Maximize();
+                ReadOnlyCollection<IWebElement> headerNameCells = driver.FindElements(By.XPath("//*[@id='myHeader']/div[2]/div"));
+                TestHelper.Pause();
+
+                Assert.Equal("Dato", headerNameCells[0].Text);
+                Assert.Equal("Kendelsesnr.", headerNameCells[1].Text);
+                Assert.Equal("Emneord", headerNameCells[2].Text);
+                Assert.Equal("Selskab", headerNameCells[3].Text);
+                Assert.Equal("Forsikringstype", headerNameCells[4].Text);
+                Assert.Equal("Medhold", headerNameCells[5].Text);
+                Assert.Equal("P.K", headerNameCells[6].Text);
+
+            }
+        }
+
+
+        [Fact]
+        public void FillInSearchParams()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Navigate to '{HomeUrl}'");
+                driver.Navigate().GoToUrl(HomeUrl);
+                driver.Manage().Window.Maximize();
+                TestHelper.Pause();
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Typing in value to input text field");
+                driver.FindElement(By.Id("txtInputAutocomplete")).SendKeys("Hund ");
+                TestHelper.Pause(1000);
+                driver.FindElement(By.Id("txtInputAutocomplete")).SendKeys("kat");
+                TestHelper.Pause(1000);
+                //Select Checkbox
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding element and clicking on it");
+                driver.FindElement(By.Id("checkboxComplainantUpheld")).Click();
+                TestHelper.Pause(1000);
+                //select from custom company dropdown
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding element and opens it by clicking on it");
+                driver.FindElement(By.Id("companyNames")).Click();
+                TestHelper.Pause(1000);
+                driver.FindElement(By.CssSelector("[data-tooltip*='Tryg Forsikring']")).Click();
+                TestHelper.Pause(1000);
+                driver.FindElement(By.CssSelector("[data-tooltip*='Alm. Brand']")).Click();
+                //TestHelper.Pause(1000);
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding element and closes it by clicking on it");
+                driver.FindElement(By.Id("companyNames")).Click();
+                TestHelper.Pause(1000);
+                ////select from custom incurance-type dropdown
+                //driver.FindElement(By.Id("insuranceType")).Click();
+                //TestHelper.Pause(1000);
+                //driver.FindElement(By.CssSelector("[data-tooltip*='Ulykke']")).Click();
+                //TestHelper.Pause(1000);
+                //driver.FindElement(By.Id("insuranceType")).Click();
+                //Select Date between
+                //driver.FindElement(By.Id("fromDate")).Click();
+                //TestHelper.Pause(1000);
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Finding element and setting the date by filling in value as a text sting");
+                driver.FindElement(By.Id("fromDate")).SendKeys("24062012");
+                //TestHelper.Pause(1000);
+                driver.FindElement(By.Id("toDate")).SendKeys("26062012");
+                //TestHelper.Pause(2000);
+                output.WriteLine($"{DateTime.Now.ToLongTimeString()} Submitting the form");
+                driver.FindElement(By.Id("btnAutocomplete")).Click();
+
+                Assert.Equal("25.06.2012", driver.FindElement(By.ClassName("saved-date")).Text);
+                Assert.Equal("81400", driver.FindElement(By.ClassName("saved-caseNumber")).Text);
+                Assert.Equal("accept af risiko, personskade, hundeansvarsforsikring, besidder", driver.FindElement(By.ClassName("saved-searchWords")).Text);
+                Assert.Equal("alm. brand", driver.FindElement(By.ClassName("saved-companyName")).Text);
+                Assert.Equal("husdyr", driver.FindElement(By.ClassName("saved-insuranceType")).Text);
+                Assert.Equal("klager medhold", driver.FindElement(By.ClassName("saved-result")).Text);
+                Assert.Equal("ja", driver.FindElement(By.ClassName("saved-principle")).Text);
+
+                TestHelper.Pause(5000);
+            }
+        }
+        
+
+        //[Fact]
+        //public void GoToFogPByXPath()
+        //{
+        //    using (IWebDriver driver = new ChromeDriver())
+        //    {
+
+        //        driver.Navigate().GoToUrl(HomeUrl);
+        //        TestHelper.Pause();
+        //        driver.Manage().Window.Maximize();
+        //        IWebElement fogpLinkXpath = driver.FindElement(By.XPath("/html/body/footer[1]/section/div/div/div[2]/div/div/div[2]"));
+
+        //        fogpLinkXpath.Click();
+
+        //        TestHelper.Pause();
+
+        //        //Assert.Equal("Privacy Policy - WebApplication1", driver.Title);
+        //        //Assert.Equal(PrivateUrl, driver.Url);
+        //    }
+        //}
     }
 }
--- SearchEngine.UITests/SmokeTestClass.cs
+++ SearchEngine.UITests/SmokeTestClass.cs
@@ -2,7 +2,12 @@
 using Xunit;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+using ApprovalTests.Reporters;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Reporters.Windows;
 
 namespace SearchEngine.UITests
 {
@@ -18,8 +23,23 @@ namespace SearchEngine.UITests
         {
             using (IWebDriver driver = new ChromeDriver()) 
             {
-
+                
                 driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause(2000);
+                driver.Manage().Window.Maximize();
+                TestHelper.Pause(2000);
+                driver.Manage().Window.Minimize();
+                TestHelper.Pause(2000);
+                driver.Manage().Window.Size = new System.Drawing.Size(300, 400);
+                TestHelper.Pause(2000);
+                driver.Manage().Window.Position = new System.Drawing.Point(0,0);
+                TestHelper.Pause(2000);
+                driver.Manage().Window.Position = new System.Drawing.Point(50, 50);
+                TestHelper.Pause(2000);
+                driver.Manage().Window.Position = new System.Drawing.Point(100, 100);
+                TestHelper.Pause(2000);
+                driver.Manage().Window.FullScreen();
+                TestHelper.Pause(2000);
 
                 //Skal fjernes. Denne bliver kun brugt til at sætte et delay, så man kan se hvad der egentlig sker, når testprogrammet åbner vores browservindue. 
                 //TestHelper.Pause();
@@ -38,7 +58,6 @@ namespace SearchEngine.UITests
                 
                 driver.Navigate().GoToUrl(HomeUrl);
                 driver.Navigate().Refresh();
-
                 Assert.Equal(HomeTitle, driver.Title);
                 Assert.Equal(HomeUrl, driver.Url);
 
@@ -53,11 +72,18 @@ namespace SearchEngine.UITests
             {
 
                 driver.Navigate().GoToUrl(HomeUrl);
-                TestHelper.Pause(100);
-                IWebElement randomNumber = driver.FindElement(By.Id("readomNumberToTestOn"));
+
+                //Den her løsning bruger Hjælper klassen - TestHelper - Det er ikke den bedste måde at sætte en pause på ved at bruge thread sleep.
+                //TestHelper.Pause(100);// Giver random number tid til at genererer et nummer, ellers bliver værdien sat til - ""
+                //IWebElement randomNumber = driver.FindElement(By.Id("readomNumberToTestOn"));
+                //string initialToken = randomNumber.Text;
+
+                // Den her løsning bruger WebDriverWait som vi får til rådighed OpenQA.Selenium.Support.UI; - Dette er en mere korekt måde at sætte et delay på
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(10));
+                IWebElement randomNumber = wait.Until((d) => d.FindElement(By.Id("readomNumberToTestOn")));
                 string initialToken = randomNumber.Text;
+
                 TestHelper.Pause();
-                Console.WriteLine(initialToken);
                 driver.Navigate().GoToUrl(PrivateUrl);
                 //TestHelper.Pause();
                 driver.Navigate().Back();
@@ -82,20 +108,76 @@ namespace SearchEngine.UITests
                 //TestHelper.Pause();
 
                 driver.Navigate().GoToUrl(HomeUrl);
-                TestHelper.Pause(100);
-                IWebElement randomNumber = driver.FindElement(By.Id("readomNumberToTestOn"));
+                //TestHelper.Pause(100); // Giver random number tid til at genererer et nummer, ellers bliver værdien sat til - ""
+                //IWebElement randomNumber = driver.FindElement(By.Id("readomNumberToTestOn"));
+                //string initialToken = randomNumber.Text;
+
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromMilliseconds(10));
+                IWebElement randomNumber = wait.Until((d) => d.FindElement(By.Id("readomNumberToTestOn")));
                 string initialToken = randomNumber.Text;
+
                 driver.Navigate().Back();
                 //TestHelper.Pause();
 
                 driver.Navigate().Forward();
-                TestHelper.Pause(100);
+                TestHelper.Pause(200);// Giver random number tid til at genererer et nummer, ellers bliver værdien sat til - ""
+
 
                 Assert.Equal(HomeTitle, driver.Title);
                 Assert.Equal(HomeUrl, driver.Url);
 
                 string reloadedToken = driver.FindElement(By.Id("readomNumberToTestOn")).Text;
                 Assert.NotEqual(initialToken, reloadedToken);
+
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "smoke")]
+        public void OpenfrequentlyAskedQuestions()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+                TestHelper.Pause(2000);
+
+                driver.FindElement(By.Name("frequentlyAskedQuestions")).Click();
+                TestHelper.Pause(2000);
+
+                ReadOnlyCollection<string> allTabs = driver.WindowHandles;
+                string homePageTab = allTabs[0];
+                string frequentlyQuestionsTab = allTabs[1];
+
+                driver.SwitchTo().Window(frequentlyQuestionsTab);
+
+                TestHelper.Pause(200);
+
+                Assert.Equal("https://ankeforsikring.dk/Sider/faq.aspx", driver.Url);
+            }
+        }
+
+
+        [Fact]
+        [Trait("Category", "smoke")]
+        [UseReporter(typeof(BeyondCompareReporter))]
+        public void ScreenShotHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(PrivateUrl);
+                driver.Manage().Window.Maximize();
+                ITakesScreenshot screenShotDriver = (ITakesScreenshot)driver;
+
+                Screenshot screenshot = screenShotDriver.GetScreenshot();
+
+                TestHelper.Pause(5000);
+
+                screenshot.SaveAsFile("privacy.bmp", ScreenshotImageFormat.Bmp);
+
+                FileInfo file = new FileInfo("privacy.bmp");
+                Approvals.Verify(file);
 
             }
         }
